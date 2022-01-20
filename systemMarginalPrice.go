8c3468a8fc9365322e03a8745d1b1c6a800f5eff
James Hughes <jim.d.hughes@gmail.com>
2022-01-19 19:02:05 -0700

    updating system marginal price services and tests

--- systemMarginalPrice.go
+++ systemMarginalPrice.go
@@ -35,60 +35,65 @@ type AesoSystemMarginalPriceResponse struct {
 	Return       AesoSystemMarginalPriceResponseReportPart `json:"return"`
 }
 
-func (a *AesoApiService) GetSystemMarginalPrice(start, end time.Time, offset int64) []MappedSystemMarginalPrice {
+func (a *AesoApiService) GetSystemMarginalPrice(start, end time.Time) ([]MappedSystemMarginalPrice, error) {
 	var response AesoSystemMarginalPriceResponse
 	var res []MappedSystemMarginalPrice
 	sDateString := start.Format("2006-01-02")
 	eDateString := end.Format("2006-01-02")
-	bytes, err := a.execute(fmt.Sprintf(AESO_API_URL_POOLPRICE, sDateString, eDateString))
+	bytes, err := a.execute(fmt.Sprintf(AESO_API_URL_SYSTEMMARGINALPRICE, sDateString, eDateString))
 	if err != nil {
 		log.Println(err)
-		return []MappedSystemMarginalPrice{}
+		return []MappedSystemMarginalPrice{}, err
 	}
 	err = json.Unmarshal(bytes, &response)
 	if err != nil {
 		log.Fatal(err)
 	}
 	for _, entry := range response.Return.Report {
-		// Date comes back as yyyy-mm-dd HH
-		parts := strings.Split(entry.DateHourEnding, " ")
-		date, err := time.Parse("01/02/2006", parts[0])
+		mappedValue, err := mapAesoSystemMarginalPriceToStruct(entry)
 		if err != nil {
-			log.Fatal(err)
+			log.Println(err)
+			return []MappedSystemMarginalPrice{}, err
 		}
-		if len(parts[1]) > 2 {
-			parts[1] = parts[1][0:1]
-		}
-		h, err := strconv.ParseInt(parts[1], 10, 64)
-		if err != nil {
-			log.Fatal(err)
-		}
-		// time comes back as a string in military time. HH:MM
-		timeParts := strings.Split(entry.Time, ":")
-		hours, err := strconv.ParseInt(timeParts[0], 10, 64)
-		if err != nil {
-			log.Fatal(err)
-		}
-		date.Add(time.Duration(hours) * time.Hour)
-		minutes, err := strconv.ParseInt(timeParts[1], 10, 64)
-		if err != nil {
-			log.Fatal(err)
-		}
-		date.Add(time.Duration(minutes) * time.Minute)
-		price, err := strconv.ParseFloat(entry.PriceInDollar, 64)
-		if err != nil {
-			log.Fatal(err)
-		}
-		volume, err := strconv.ParseFloat(entry.VolumeInMW, 64)
-		if err != nil {
-			log.Fatal(err)
-		}
-		res = append(res, MappedSystemMarginalPrice{
-			Date:       date,
-			HourEnding: h,
-			Price:      price,
-			VolumeInMW: volume,
-		})
+		res = append(res, mappedValue)
 	}
-	return res
+	return res, nil
+}
+
+func mapAesoSystemMarginalPriceToStruct(entry AesoSystemMarginalPriceReport) (MappedSystemMarginalPrice, error) {
+	// Date comes back as yyyy-mm-dd HH
+	// But we also get a time in the format HH:MM
+	parts := strings.Split(entry.DateHourEnding, " ")
+	datePartString := parts[0]
+	timePartsString := parts[1]
+	fullDateString := fmt.Sprintf("%s %s:00", datePartString, entry.Time)
+	date, err := ConvertAesoDateToUTC(fullDateString)
+	if err != nil {
+		log.Println(err)
+		log.Printf("Error converting %s from Mountain to UTC\n", fullDateString)
+		return MappedSystemMarginalPrice{}, err
+	}
+
+	h, err := strconv.ParseInt(timePartsString, 10, 64)
+	if err != nil {
+		log.Println(err)
+		return MappedSystemMarginalPrice{}, err
+	}
+
+	price, err := strconv.ParseFloat(entry.PriceInDollar, 64)
+	if err != nil {
+		log.Println(err)
+		return MappedSystemMarginalPrice{}, err
+	}
+	volume, err := strconv.ParseFloat(entry.VolumeInMW, 64)
+	if err != nil {
+		log.Println(err)
+		return MappedSystemMarginalPrice{}, err
+	}
+	return MappedSystemMarginalPrice{
+		Date:       date,
+		HourEnding: h,
+		Price:      price,
+		VolumeInMW: volume,
+	}, nil
 }
