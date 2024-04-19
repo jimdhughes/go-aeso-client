8c7c9df02e69be4d0343245799d92241a95221c7
James Hughes <jim.d.hughes@gmail.com>
2024-04-18 22:27:59 -0600

    adding support to search by participant ids and names

--- poolParticipant.go
+++ poolParticipant.go
@@ -2,7 +2,7 @@ package aeso
 
 import "encoding/json"
 
-const AESO_API_URL_POOLPARTICIPANT = "https://api.aeso.ca/report/v1.1/price/poolPrice?startDate=%s&endDate=%s"
+const AESO_API_URL_POOLPARTICIPANT = "https://api.aeso.ca/report/v1/poolparticipantlist"
 
 type ParticipantPoolEntryAgent struct {
 	ID   string `json:"agent_ID"`
@@ -30,6 +30,39 @@ func (a *AesoApiService) GetPoolPriceParticpant() ([]PoolParticipantEntry, error
 	if err != nil {
 		return res, err
 	}
+	err = json.Unmarshal(bytes, &aesoRes)
+	if err != nil {
+		return []PoolParticipantEntry{}, err
+	}
+	return aesoRes.Return, nil
+}
+
+// GetPoolPriceByParticipantIDsorNames retrieves the pool price participant data from the AESO API by participant IDs or names.
+func (a *AesoApiService) GetPoolPriceByParticipantIDsorNames(participantIDs, participantNames []string) ([]PoolParticipantEntry, error) {
+	currUrl := AESO_API_URL_POOLPARTICIPANT
+	if len(participantIDs) > 0 {
+		currUrl += "?participantID="
+		for i, id := range participantIDs {
+			if i > 0 {
+				currUrl += ","
+			}
+			currUrl += id
+		}
+	}
+	if len(participantNames) > 0 {
+		currUrl += "?participantName="
+		for i, name := range participantNames {
+			if i > 0 {
+				currUrl += ","
+			}
+			currUrl += name
+		}
+	}
+	bytes, err := a.execute(currUrl)
+	if err != nil {
+		return []PoolParticipantEntry{}, err
+	}
+	var aesoRes PoolParticipantResponse
 	err = json.Unmarshal(bytes, &aesoRes)
 	if err != nil {
 		return []PoolParticipantEntry{}, err
