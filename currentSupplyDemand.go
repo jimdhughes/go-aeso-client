dd4a5e105b657979ce2a5a7fecf09aa21ca74083
James Hughes <jim.d.hughes@gmail.com>
2024-10-13 08:22:28 -0600

    fixing bug

--- currentSupplyDemand.go
+++ currentSupplyDemand.go
@@ -5,7 +5,7 @@ import "encoding/json"
 const AESO_API_URL_CURRENT_SUPPLY_DEMAND_SUMMARY = "https://api.aeso.ca/report/v1/csd/summary/current"
 
 type InterchangeEntry struct {
-	Path       string `json:"string"`
+	Path       string `json:"path"`
 	ActualFlow int32  `json:"actual_flow"`
 }
 
